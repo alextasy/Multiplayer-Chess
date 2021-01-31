725e340da298e2958237d961fd485db698cce996
alextasy <alex05tmm@gmail.com>
2021-02-01 00:05:58 +0200

    feat(frontend): create horizontal line component

---
+++ frontend/src/app/components/horizontal-line/HorizontalLine.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './HorizontalLine.scss';
+
+function HorizontalLine(props) {
+    return <div className='HorizontalLine' style={{width: props.width}}></div>
+}
+
+export default HorizontalLine;
