2be8656a789e96fdcd3cfb33d4e908537d229000
alextasy <alex05tmm@gmail.com>
2021-01-31 23:09:01 +0200

    feat(frontend): set up intial routes and create HomePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.scss';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './app/pages/home-page/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi there!</h1>
+      <Switch>
+        <Route path='/' exact component={HomePage}/>
+      </Switch>
     </div>
   );
 }
---
+++ frontend/src/app/pages/home-page/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomePage.scss';
+
+function HomePage() {
+    return (
+        <div className='HomePage'>
+
+        </div>
+    )
+}
+
+export default HomePage;
