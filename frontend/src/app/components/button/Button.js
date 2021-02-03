b5fff5705c78553a1455f81952c7c59f8245f3d4
alextasy <alex05tmm@gmail.com>
2021-02-03 03:13:39 +0200

    refactor(Button): color application and hover effect

--- frontend/src/app/components/button/Button.js
+++ frontend/src/app/components/button/Button.js
@@ -2,21 +2,8 @@ import React from 'react'
 import './Button.scss';
 
 function Button({ color, children, click }) {
-    let border = null;
-    const bgColor = color ? applyColor() : null;
-
-    function applyColor() {
-        switch(color){
-            case 'primary': border = '2px solid #537133';return '#7fa650';
-            case 'backgroundDark': return '#272522';
-            case 'highlight': return '#44413f';
-            //returns background color by default
-            default: return '#312e2b';
-        };
-    }
-
     return (
-        <div className='Button' style={{ backgroundColor: bgColor, borderBottom: border }} onClick={ click }>
+        <div className={ `Button ${color}` } onClick={ click }>
             { children }
         </div>
     )
