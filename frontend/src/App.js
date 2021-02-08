c812d0d06da20b97b2fa98cce4ec4cc903268cb9
alextasy <alex05tmm@gmail.com>
2021-02-09 01:13:09 +0200

    refactor(app): move sidemenu into app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import './App.scss';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './app/pages/home-page/HomePage';
+import Local from './app/pages/local/Local';
+import SideMenu from './app/components/side-menu/SideMenu';
 
 function App() {
   return (
     <div className="App">
+      <SideMenu />
+      <div className='container'>
       <Switch>
         <Route path='/' exact component={HomePage}/>
+        <Route path='/local' exact component={Local}/>
       </Switch>
+      </div>
     </div>
   );
 }
--- frontend/src/app/pages/home-page/HomePage.js
+++ frontend/src/app/pages/home-page/HomePage.js
@@ -9,22 +9,19 @@ import HorizontalLine from '../../components/horizontal-line/HorizontalLine';
 function HomePage({ history }) {
     return (
         <div className='HomePage'>
-            <SideMenu />
-            <div className='container'>
-                <div className='hero-section'>
-                    <h1>FREE ONLINE CHESS</h1>
-                    <h2>PLAY EASILY WITH ANYONE YOU CHOOSE</h2>
-                    <p>Invite your friends and play some chess online! Feel free to
-                        <b onClick={()=> history.push('/sign-up')}> SIGN UP</b> to get cool new features such as having a custom display name, tracking your match history and personal statistics! </p>
-                    <p>Playing as a guest is also an option - no strings attached. You can hop on and play a quick game anonymously anytime you wish!</p>
-                    <div>
-                        <Button click={()=> history.push('/sign-in')} color='primary'>SIGN IN</Button>
-                        <Button click={()=> history.push('/sign-in')} color='highlight'>PLAY AS A GUEST</Button>
-                    </div>
+            <div className='hero-section'>
+                <h1>FREE ONLINE CHESS</h1>
+                <h2>PLAY EASILY WITH ANYONE YOU CHOOSE</h2>
+                <p>Invite your friends and play some chess online! Feel free to
+                    <b onClick={()=> history.push('/sign-up')}> SIGN UP</b> to get cool new features such as having a custom display name, tracking your match history and personal statistics! </p>
+                <p>Playing as a guest is also an option - no strings attached. You can hop on and play a quick game anonymously anytime you wish!</p>
+                <div>
+                    <Button click={()=> history.push('/sign-in')} color='primary'>SIGN IN</Button>
+                    <Button click={()=> history.push('/sign-in')} color='highlight'>PLAY AS A GUEST</Button>
                 </div>
-                <Board />
-            <HorizontalLine width='70%'/>
             </div>
+            <Board />
+            <HorizontalLine width='70%'/>
         </div>
     )
 }
