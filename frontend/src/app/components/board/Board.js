84f666d03eeed98647b60aebdf3e812d48c08e7c
alextasy <alex05tmm@gmail.com>
2021-02-09 00:51:54 +0200

    feat(board): add basic movement

--- frontend/src/app/components/board/Board.js
+++ frontend/src/app/components/board/Board.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Square from './board-logic/square';
+import Square from './board-elements/square';
 import './Board.scss';
 
 function initialSetUp() {
@@ -14,39 +14,76 @@ function initialSetUp() {
     return arr;
 }
 
-function Board({ playerIsBlack }) {
-    const [gameBoardState, setGameBoardState] = useState(initialSetUp());
+function Board({ playerIsBlack, isMyTurn = true, playable = true }) {
+    const [gameBoard, setGameBoard] = useState(initialSetUp());
     const [selectedFigure, setSelectedFigure] = useState(null);
-    const [isMyTurn, setIsMyTurn] = useState(playerIsBlack ? false : true);
+    const [currentTurn, setCurrentTurn] = useState('white');
     const [gameIsOver, setGameIsOver] = useState(null);
+    const [availableMoves, setAvailableMoves] = useState (null);
 
     function selectFigure(square) {
+        if (square.occupiedBy?.color !== currentTurn || !isMyTurn) return;
         console.log(square);
+
+        toggleSelectedStyles(square.occupiedBy);
+        setSelectedFigure(square.occupiedBy);
+        setAvailableMoves(square.occupiedBy.getAvailableMoves(gameBoard));
     }
 
     function moveFigure(square) {
-        console.log(square);
+        toggleSelectedStyles(selectedFigure);
+
+        if (square.position !== selectedFigure.position && square.occupiedBy?.color === currentTurn) {
+            selectFigure(square);
+            return;
+        }
+        if (square.position === selectedFigure.position || !availableMoves.includes(square.position)) {
+            setSelectedFigure(null);
+            setAvailableMoves(null);
+            return;
+        }
+        const gameBoardCopy = [...gameBoard];
+        gameBoardCopy[selectedFigure.position -1].occupiedBy = null;
+        selectedFigure.position = square.position;
+        gameBoardCopy[square.position -1].occupiedBy = selectedFigure;
+        setSelectedFigure(null);
+        setAvailableMoves(null);
+        setGameBoard(gameBoardCopy);
     }
 
+    function toggleSelectedStyles(figure) {
+        const toggleClass = (className, id) => document.getElementById(id).classList.toggle(className);
+        const positions = figure.getAvailableMoves(gameBoard);
+        toggleClass('selected', figure.position);
+        positions.forEach(position => {
+            if (gameBoard[position -1].occupiedBy) toggleClass('potential-move-take', position);
+            else toggleClass('potential-move', position)
+        });
+    }
 
-    const gameBoard = gameBoardState.map(square => (
+    const gameBoardElement = gameBoard.map(square => (
         <div
             className='square'
             style={{ backgroundColor: square.color }}
+            id= { square.position }
             key={ square.position }
-            onClick={ () => selectedFigure ? moveFigure(square) : selectFigure(square) }
-            >
-            { square.occupiedBy ?
+            onClick={ () => {
+                if (!playable) return;
+                selectedFigure ? moveFigure(square) : selectFigure(square) }
+            }>
+            {
+                square.occupiedBy ?
                 <img
                     src={ square?.occupiedBy?.img?.src }
                     alt={ square?.occupiedBy?.img?.alt }
                     style={{ transform: playerIsBlack ? 'rotate(180deg)' : null }}>
-                </img> : null }
+                </img> : null
+            }
         </div>));
 
     return (
         <div className='Board' style={{ transform: playerIsBlack ? 'rotate(180deg)' :null }}>
-            { gameBoard }
+            { gameBoardElement }
         </div>
     )
 }
