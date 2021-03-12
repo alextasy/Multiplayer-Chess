fbea2a950268fb139d0217603b94e38a813a7cf4
alextasy <alex05tmm@gmail.com>
2021-03-13 01:32:33 +0200

    feat(square): add square name

--- frontend/src/app/components/board/board-elements/square.js
+++ frontend/src/app/components/board/board-elements/square.js
@@ -18,6 +18,7 @@ export default class Square {
         this.color = color;
         this.occupiedBy = null;
         this.intialSetUp(position);
+        this.name = `${this.getSquareLetter(position)}${this.getSquareNumber(position)}`;
     }
 
     intialSetUp(p) {
@@ -36,4 +37,27 @@ export default class Square {
         if (p === 60) this.occupiedBy = new Figure('white', 'queen', { src: wQueen, alt: 'White queen' }, p);
         if (p === 61) this.occupiedBy = new Figure('white', 'king', { src: wKing, alt: 'White king' }, p);
     }
+
+    getSquareLetter(p) {
+        if ([1, 9, 17, 25, 33, 41, 49, 57].includes(p)) return 'a';
+        if ([2, 10, 18, 26, 34, 42, 50, 58].includes(p)) return 'b';
+        if ([3, 11, 19, 27, 35, 43, 51, 59].includes(p)) return 'c';
+        if ([4, 12, 20, 28, 36, 44, 52, 60].includes(p)) return 'd';
+        if ([5, 13, 21, 29, 37, 45, 53, 61].includes(p)) return 'e';
+        if ([6, 14, 22, 30, 38, 46, 54, 62].includes(p)) return 'f';
+        if ([7, 15, 23, 31, 39, 47, 55, 63].includes(p)) return 'g';
+        if ([8, 16, 24, 32, 40, 48, 56, 64].includes(p)) return 'h';
+    }
+
+    getSquareNumber(p) {
+        if (p <= 8) return 8;
+        if (p <= 16) return 7;
+        if (p <= 24) return 6;
+        if (p <= 32) return 5;
+        if (p <= 40) return 4;
+        if (p <= 48) return 3;
+        if (p <= 56) return 2;
+        return 1;
+    }
+
 }
