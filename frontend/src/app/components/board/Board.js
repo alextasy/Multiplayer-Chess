22f750fd058db8ed70a870415507ee06f90ec508
alextasy <alex05tmm@gmail.com>
2021-03-13 14:25:10 +0200

    feat(board): add basic checked logic and style refactor the html part slighly as well

--- frontend/src/app/components/board/Board.js
+++ frontend/src/app/components/board/Board.js
@@ -12,12 +12,17 @@ function Board({ playerIsBlack, isMyTurn = true, playable = true, location }) {
     const isLocal = useRef(location.pathname === '/local');
     const [blackFigures, setBlackFigures] = useState(setFigures('black'));
     const [whiteFigures, setWhiteFigures] = useState(setFigures('white'));
+    const [checkedPlayer, setCheckedPlayer] = useState(null);
+
+    if (checkedPlayer) {
+
+    }
 
     function selectFigure(square) {
         if (square.occupiedBy?.color !== currentTurn || !isMyTurn) return;
         toggleSelectedStyles(square.occupiedBy);
         setSelectedFigure(square.occupiedBy);
-        setAvailableMoves(square.occupiedBy.getAvailableMoves(gameBoard));
+        setAvailableMoves(square.occupiedBy.getDefaultMoves(gameBoard));
     }
 
     function moveFigure(square) {
@@ -37,19 +42,30 @@ function Board({ playerIsBlack, isMyTurn = true, playable = true, location }) {
            if (currentTurn === 'white') setBlackFigures(blackFigures.filter(figure => figure !== square.occupiedBy));
            else setWhiteFigures(whiteFigures.filter(figure => figure !== square.occupiedBy));
         }
+        document.querySelector('.checked')?.classList.remove('checked');
         gameBoardCopy[selectedFigure.position -1].occupiedBy = null;
         selectedFigure.position = square.position;
         gameBoardCopy[square.position -1].occupiedBy = selectedFigure;
+        seeIfCheck(gameBoardCopy, square.occupiedBy);
         setSelectedFigure(null);
         setAvailableMoves(null);
         setGameBoard(gameBoardCopy);
         switchTurn();
-        console.log({blackFigures: blackFigures, whiteFigures: whiteFigures});
+    }
+
+    function seeIfCheck(board, figure) {
+        const nextPositions = figure.getDefaultMoves(board);
+        nextPositions.forEach(position => {
+            if (board[position - 1].occupiedBy?.type === 'king') {
+                document.getElementById(position).classList.add('checked');
+                setCheckedPlayer(currentTurn === 'white' ? 'black' : 'white');
+            }
+        });
     }
 
     function toggleSelectedStyles(figure) {
         const toggleClass = (className, id) => document.getElementById(id).classList.toggle(className);
-        const positions = figure.getAvailableMoves(gameBoard);
+        const positions = figure.getDefaultMoves(gameBoard);
         toggleClass('selected', figure.position);
         positions.forEach(position => {
             if (gameBoard[position -1].occupiedBy) toggleClass('potential-move-take', position);
@@ -63,30 +79,28 @@ function Board({ playerIsBlack, isMyTurn = true, playable = true, location }) {
             return;
         }
     }
-
-    const gameBoardElement = gameBoard.map(square => (
-        <div
-            className='square'
-            style={{ backgroundColor: square.color }}
-            id={ square.position }
-            key={ square.position }
-            onClick={ () => {
-                if (!playable) return;
-                selectedFigure ? moveFigure(square) : selectFigure(square) }
-            }>
-            {
-                square.occupiedBy ?
-                <img
-                    src={ square?.occupiedBy?.img?.src }
-                    alt={ square?.occupiedBy?.img?.alt }
-                    style={{ transform: playerIsBlack ? 'rotate(180deg)' : null }}>
-                </img> : null
-            }
-        </div>));
-
+    //HTML
     return (
         <div className='Board' style={{ transform: playerIsBlack ? 'rotate(180deg)' :null }}>
-            { gameBoardElement }
+            { gameBoard.map(square => (
+                <div className='square'
+                    style={{ backgroundColor: square.color }}
+                    id={ square.position }
+                    key={ square.position }
+                    onClick={ () => {
+                        if (!playable) return;
+                        selectedFigure ? moveFigure(square) : selectFigure(square) }
+                    }>
+                    {
+                        square.occupiedBy ?
+                        <img
+                            src={ square?.occupiedBy?.img?.src }
+                            alt={ square?.occupiedBy?.img?.alt }
+                            style={{ transform: playerIsBlack ? 'rotate(180deg)' : null }}>
+                        </img> : null
+                    }
+                </div>))
+            }
         </div>
     )
 }
