6d05c1b9d52f56729e82644e164d280947272efb
alextasy <alex05tmm@gmail.com>
2021-03-13 02:24:15 +0200

    feat(local): add a local game mode

---
+++ frontend/src/app/pages/local/Local.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from '../../components/board/Board';
+import './Local.scss';
+
+function Local() {
+    return (
+        <div className='Local'>
+            <Board />
+        </div>
+    )
+}
+
+export default Local;
