5d6033dda03c40b6287b9fb4bc82348796aacba7
alextasy <alex05tmm@gmail.com>
2021-03-29 00:49:32 +0300

    feat(multiplayer): create basic layout

--- frontend/src/app/pages/multiplayer/Multiplayer.js
+++ frontend/src/app/pages/multiplayer/Multiplayer.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Aside from '../../components/aside/Aside';
+import Board from '../../components/board/Board';
 import './Multiplayer.scss';
 
 function Multiplayer() {
     return (
-        <div>
+        <div className='Multiplayer'>
+            <Board />
+            <Aside>
 
+            </Aside>
         </div>
     )
 }
