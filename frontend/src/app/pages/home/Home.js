447bdd7afb888d794d4703d5e785c58ff22025f6
alextasy <alex05tmm@gmail.com>
2021-04-01 01:11:11 +0300

    refactor(home): change signup url style

--- frontend/src/app/pages/home/Home.js
+++ frontend/src/app/pages/home/Home.js
@@ -13,11 +13,11 @@ function Home({ history }) {
                 <h1>FREE ONLINE CHESS</h1>
                 <h2>PLAY EASILY WITH ANYONE YOU CHOOSE</h2>
                 <p>Invite your friends and play some chess online! Feel free to
-                    <b onClick={()=> history.push('/sign-up')}> SIGN UP</b> to get cool new features such as having a custom display name, tracking your match history and personal statistics! </p>
+                    <b onClick={()=> history.push('/signup')}> SIGN UP</b> to get cool new features such as having a custom display name, tracking your match history and personal statistics! </p>
                 <p>Playing as a guest is also an option - no strings attached. You can hop on and play a quick game anonymously anytime you wish!</p>
                 <div>
                     <Button click={()=> signInState.setSingingIn(true) } color='primary'>SIGN IN</Button>
-                    <Button click={()=> history.push('/sign-in') } color='highlight'>PLAY AS A GUEST</Button>
+                    <Button click={()=> history.push('/multiplayer') } color='highlight'>PLAY AS A GUEST</Button>
                 </div>
             </div>
             <Board playable={ false }/>
