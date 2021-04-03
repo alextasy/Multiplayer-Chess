0d855917437fc072d7f6204afa6935e4e56eaf65
alextasy <alex05tmm@gmail.com>
2021-04-03 19:22:09 +0300

    chore(index): move app context provider to be global

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
+import AppContextProvider from './app/context/AppContext';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AppContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
