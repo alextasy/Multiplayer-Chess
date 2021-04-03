07194a523480ebe92445e5ca59e96aa56dbc4b1c
alextasy <alex05tmm@gmail.com>
2021-04-03 19:30:32 +0300

    feat(app): blackout rest of app when auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,32 @@ import Local from './app/pages/local/Local';
 import SideMenu from './app/components/side-menu/SideMenu';
 import Multiplayer from './app/pages/multiplayer/Multiplayer';
 import GameContextProvider from './app/context/GameContext';
-import AppContextProvider from './app/context/AppContext';
+import { AppContext } from './app/context/AppContext';
+import { useContext, useEffect, useRef } from 'react';
 
 function App() {
+  const { isSigningIn, isSigningUp } = useContext(AppContext);
+  const overlay = useRef(null);
+  const initialLoad = useRef(true);
+
+  useEffect(() => {
+    if (initialLoad.current) return initialLoad.current = false;
+    overlay.current.classList.toggle('active');
+  }, [isSigningIn, isSigningUp])
+
   return (
     <GameContextProvider>
-    <AppContextProvider>
       <div className="App">
         <SideMenu />
         <div className='container'>
-        <Switch>
-          <Route path='/' exact component={ Home }/>
-          <Route path='/local' exact component={ Local }/>
-          <Route path='/multiplayer' exact component={ Multiplayer }/>
-        </Switch>
+          <Switch>
+            <Route path='/' exact component={ Home }/>
+            <Route path='/local' exact component={ Local }/>
+            <Route path='/multiplayer' exact component={ Multiplayer }/>
+          </Switch>
+          <div className='authOverlay' ref={ overlay }></div>
         </div>
       </div>
-    </AppContextProvider>
     </GameContextProvider>
   );
 }
