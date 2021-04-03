9e3bc8adad7a8793bfa6445f9acbad982e539673
alextasy <alex05tmm@gmail.com>
2021-04-03 15:56:04 +0300

    feat(side-menu): add sign up to side menu

--- frontend/src/app/components/side-menu/SideMenu.js
+++ frontend/src/app/components/side-menu/SideMenu.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import './SideMenu.scss';
 import logo from '../../../assets/icons/logo.png';
 import HorizontalLine from '../horizontal-line/HorizontalLine';
@@ -6,6 +6,7 @@ import Button from '../button/Button';
 import { withRouter } from 'react-router-dom';
 import { AppContext } from '../../context/AppContext';
 import SignIn from '../sign-in/SignIn';
+import SignUp from '../sign-up/SignUp';
 
 function SideMenu({ history }) {
     const { isSigningIn, isSigningUp, setIsSigningIn, setIsSigningUp } = useContext(AppContext);
@@ -14,7 +15,7 @@ function SideMenu({ history }) {
         <div className='SideMenu'>
             <img src={ logo } alt='logo' onClick={ ()=> history.push('/') } style={{ cursor: 'pointer' }}/>
             <HorizontalLine />
-            <section className={ isSigningIn ? 'hidden' : ''}>
+            <section className={ isSigningIn || isSigningUp ? 'hidden' : ''}>
                 <h3>PLAY AS GUEST</h3>
                 <Button click={() => history.push('/local') }>LOCAL MULTIPLAYER</Button>
                 <Button click={() => history.push('/multiplayer') }>ONLINE MULTIPLAYER</Button>
@@ -23,9 +24,10 @@ function SideMenu({ history }) {
                 <Button click={() => history.push('/history') }>MATCH HISTORY</Button>
                 <Button click={() => history.push('/profile') }>MY PROFILE</Button>
                 <Button click={() => setIsSigningIn(true) } color='primary'>SIGN IN</Button>
-                <p onClick={() => history.push('/signup') }>Don’t have an account? Sign up!</p>
+                <p onClick={() => setIsSigningUp(true) }>Don’t have an account? Sign up!</p>
             </section>
             { isSigningIn ? <SignIn /> : null }
+            { isSigningUp ? <SignUp /> : null }
             <p className='quote'>“Even a poor plan is better than no plan at all.”</p>
         </div>
     )
