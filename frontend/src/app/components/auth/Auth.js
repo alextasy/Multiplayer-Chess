51e64ee65ef657e4b3e740553c164c8416dfa337
alextasy <alex05tmm@gmail.com>
2021-04-03 16:37:19 +0300

    chore(auth): remove unsused property

--- frontend/src/app/components/auth/Auth.js
+++ frontend/src/app/components/auth/Auth.js
@@ -10,7 +10,7 @@ function SignUp() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [repeatPassword, setRepeatPassword] = useState('');
-    const { isSigningIn, setIsSigningIn, isSigningUp, setIsSigningUp } = useContext(AppContext);
+    const { setIsSigningIn, isSigningUp, setIsSigningUp } = useContext(AppContext);
     const authRef = useRef();
     const transitionTime = 250;
     const transitionDelay = 400;
