a47a063f800191c41aa8160ed9beb56d4ada48ae
alextasy <alex05tmm@gmail.com>
2021-04-03 15:53:35 +0300

    feat(side-menu): extract sign in and collapse arrow

--- frontend/src/app/components/side-menu/SideMenu.js
+++ frontend/src/app/components/side-menu/SideMenu.js
@@ -1,48 +1,20 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect, useContext } from 'react';
 import './SideMenu.scss';
 import logo from '../../../assets/icons/logo.png';
 import HorizontalLine from '../horizontal-line/HorizontalLine';
 import Button from '../button/Button';
 import { withRouter } from 'react-router-dom';
-import Input from '../input/Input';
-
-export const signInState = { setSigningIn: () => {} };
+import { AppContext } from '../../context/AppContext';
+import SignIn from '../sign-in/SignIn';
 
 function SideMenu({ history }) {
-    const [signingIn, setSigningIn] = useState(false);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const collapseArrowRef = useRef(null);
-    const intervalRef = useRef(null);
-    const signInSectionRef = useRef(null);
-
-    const transitionTime = 250;
-    const transitionDelay = 400;
-
-    signInState.setSingingIn = setSigningIn;
-
-    useEffect(() => {
-        if (!signingIn) return clearInterval(intervalRef.current);
-        setTimeout(() => {
-            document.getElementById('email').focus();
-            intervalRef.current = setInterval(() => collapseArrowRef.current.classList.toggle('active'), 750);
-        }, transitionDelay + transitionTime);
-    }, [signingIn]);
-
-    const collapseFunction = () => {
-        signInSectionRef.current.style.transitionDelay = '0ms';
-        signInSectionRef.current.classList.remove('active');
-        setTimeout(() => {
-            signInSectionRef.current.style.transitionDelay = `${transitionDelay}ms`;
-            setSigningIn(false)
-        }, transitionDelay);
-    }
+    const { isSigningIn, isSigningUp, setIsSigningIn, setIsSigningUp } = useContext(AppContext);
 
     return (
         <div className='SideMenu'>
             <img src={ logo } alt='logo' onClick={ ()=> history.push('/') } style={{ cursor: 'pointer' }}/>
             <HorizontalLine />
-            <section className={ signingIn ? 'hidden' : ''}>
+            <section className={ isSigningIn ? 'hidden' : ''}>
                 <h3>PLAY AS GUEST</h3>
                 <Button click={() => history.push('/local') }>LOCAL MULTIPLAYER</Button>
                 <Button click={() => history.push('/multiplayer') }>ONLINE MULTIPLAYER</Button>
@@ -50,30 +22,10 @@ function SideMenu({ history }) {
                 <h3>SIGN IN TO USE</h3>
                 <Button click={() => history.push('/history') }>MATCH HISTORY</Button>
                 <Button click={() => history.push('/profile') }>MY PROFILE</Button>
-                <Button click={() => setSigningIn(true) } color='primary'>SIGN IN</Button>
+                <Button click={() => setIsSigningIn(true) } color='primary'>SIGN IN</Button>
                 <p onClick={() => history.push('/signup') }>Don’t have an account? Sign up!</p>
             </section>
-            <div
-                ref={ signInSectionRef }
-                className={ `sign_in ${signingIn ? 'active' : ''}` }>
-                <form autoComplete="off">
-                    <Input
-                        type='email'
-                        id='email'
-                        changeState={[ email, setEmail ]}>Email
-                    </Input>
-                    <Input
-                        type='password'
-                        id='password'
-                        changeState={[ password, setPassword ]}>Password
-                    </Input>
-                    <Button color='primary'>SIGN IN</Button>
-                    <div className='collapse' onClick={ collapseFunction }>
-                        <p>COLLAPSE</p>
-                        <p id='arrow' ref={ collapseArrowRef }></p>
-                    </div>
-                </form>
-            </div>
+            { isSigningIn ? <SignIn /> : null }
             <p className='quote'>“Even a poor plan is better than no plan at all.”</p>
         </div>
     )
