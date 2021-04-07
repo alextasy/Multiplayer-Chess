65587feda89a07cca1338f47c19cefb128dec1c3
alextasy <alex05tmm@gmail.com>
2021-04-08 01:00:23 +0300

    feat(home): add auth state to home page

--- frontend/src/app/pages/home/Home.js
+++ frontend/src/app/pages/home/Home.js
@@ -7,18 +7,44 @@ import { AppContext } from '../../context/AppContext';
 import HorizontalLine from '../../components/horizontal-line/HorizontalLine';
 
 function Home({ history }) {
-    const appContext = useContext(AppContext);
+    const { setIsSigningIn, setIsSigningUp, user, isAuth } = useContext(AppContext);
+
+    const defaultFirstP = <p>
+        Invite your friends and play some chess online! Feel free to
+        <b onClick={() => setIsSigningUp(true) }> SIGN UP</b> to
+        get cool new features such as having a custom display name,
+        tracking your match history and personal statistics!
+    </p>
+
+    const authFirstP = <p>
+        Thank you for signing in { user?.displayName }! Feel free to <b>VIEW </b>
+        and personalize your profile and explore all of our features.
+        You can also analyze your games and keep track
+        in our <b> MATCH HISTORY </b> tab.</p>
+
+    const defaultSecondP = <p>
+        Playing as a guest is also an option - no strings attached.
+        You can hop on and play a quick game anonymously anytime you wish!</p>
+
+    const authSecondP = <p>
+        Feeling ready to play some chess? No need to wait any further!
+        Explore available rooms or create one yourself. And remember to have fun!
+    </p>
+
     return (
         <div className='HomePage'>
             <div className='hero-section'>
                 <h1>FREE ONLINE CHESS</h1>
                 <h2>PLAY EASILY WITH ANYONE YOU CHOOSE</h2>
-                <p>Invite your friends and play some chess online! Feel free to
-                    <b onClick={() => appContext.setIsSigningUp(true) }> SIGN UP</b> to get cool new features such as having a custom display name, tracking your match history and personal statistics! </p>
-                <p>Playing as a guest is also an option - no strings attached. You can hop on and play a quick game anonymously anytime you wish!</p>
+                { isAuth ? authFirstP : defaultFirstP }
+                { isAuth ? authSecondP : defaultSecondP }
                 <div>
-                    <Button click={() => appContext.setIsSigningIn(true) } color='primary'>SIGN IN</Button>
-                    <Button click={()=> history.push('/multiplayer') } color='highlight'>PLAY AS A GUEST</Button>
+                    <Button
+                        click={() => isAuth ? history.push('/multiplayer') : setIsSigningIn(true) }
+                        color='primary'>{ isAuth ? 'PLAY NOW' : 'SIGN IN' }</Button>
+                    <Button
+                        click={()=> history.push('/multiplayer') }
+                        color='highlight'>{ isAuth ? 'PERSONALIZE' : 'PLAY AS A GUEST' }</Button>
                 </div>
             </div>
             <Board playable={ false }/>
