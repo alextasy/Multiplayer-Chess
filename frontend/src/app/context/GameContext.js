5e845bfc674a3376c16e4070771fab198c40bb80
alextasy <alex05tmm@gmail.com>
2021-04-11 01:30:38 +0300

    feat(game-context): add room id context

--- frontend/src/app/context/GameContext.js
+++ frontend/src/app/context/GameContext.js
@@ -3,14 +3,17 @@ import React, { useState } from 'react'
 export const GameContext = React.createContext({
     movesHistory: [],
     setMovesHistory: () => {},
+    roomId: '',
+    setRoomId: () => {},
 })
 
 const GameContextProvider = (props)=>{
     const [moves, setMoves] = useState([]);
-
+    const [room, setRoom] =useState('');
     return(
         <GameContext.Provider value={{
-            movesHistory: moves, setMovesHistory: setMoves
+            movesHistory: moves, setMovesHistory: setMoves,
+            roomId: room, setRoomId: setRoom,
          }}>
             {props.children}
         </GameContext.Provider>
