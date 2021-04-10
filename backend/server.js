82505462d45c9e062716e72b484b7c36a9b99f18
alextasy <alex05tmm@gmail.com>
2021-04-10 15:16:33 +0300

    feat(backend): set up socket io

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
-const cors = require('cors');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, { cors: { origin: '*', methods: '*' } });
 
+require('./routes/Socket')(io);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors()); // Take care of cors issues
-
 app.use('/', require('./routes/Auth').router);
 
-app.listen(process.env.PORT || 4000);
+server.listen(process.env.PORT || 4000);
