ca098658a6d75dfd90cee506fca51ddf88ea3859
alextasy <alex05tmm@gmail.com>
2021-04-11 03:40:48 +0300

    feat(socket): implement chating

--- backend/routes/Socket.js
+++ backend/routes/Socket.js
@@ -8,13 +8,19 @@ module.exports = io => {
         socket.on('createRoom', options => {
             rooms.push({ ...options, id: socket.id });
             socket.join(socket.id);
+            socket.emit('roomJoined', socket.id);
             socket.broadcast.emit('updateRooms', rooms);
         })
 
         socket.on('joinRoom', roomId => {
             socket.join(roomId);
+            socket.emit('roomJoined', roomId);
             rooms = rooms.filter(room => room.id !== roomId);
             io.emit('updateRooms', rooms);
+        })
+
+        socket.on('message', ({ message, sender, roomId }) => {
+            io.to(roomId).emit('message', { message, sender });
         })
     });
 }
